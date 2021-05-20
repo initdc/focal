74c45cfc258c0ccc1803bec5e32f9c1497b06976
Seth Forshee <seth.forshee@canonical.com>
2021-05-20 16:15:13 -0500

    UBUNTU: [Debian] run install-$(flavour) targets during build phase
    BugLink: https://bugs.launchpad.net/bugs/1930713
    BugLink: https://bugs.launchpad.net/bugs/1939287
    
    Move installation of files from the flavour build directories to the
    build phase. This results in cleaning up of one flavour build
    directory before starting the build of the next flavour, significantly
    reducing the amount of space needed on builders.
    
    Note that this will result in incorrect ownership of files in cases
    where the build and binary phases of building packages are run
    separately. This will be addressed in a later commit.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>
    Acked-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- debian/rules.d/2-binary-arch.mk
+++ debian/rules.d/2-binary-arch.mk
@@ -42,7 +42,7 @@ $(stampdir)/stamp-prepare-tree-%: $(commonconfdir)/config.common.$(family) $(arc
 prepare-%: $(stampdir)/stamp-prepare-%
 	@echo Debug: $@
 # Used by developers to allow efficient pre-building without fakeroot.
-build-%: $(stampdir)/stamp-build-%
+build-%: $(stampdir)/stamp-install-%
 	@echo Debug: $@
 
 # Do the actual build, including image and modules
@@ -804,7 +804,7 @@ ifeq ($(any_signed),true)
 	dpkg-distaddfile $(signing_tar) raw-signing -
 endif
 
-build-arch-deps-$(do_flavour_image_package) += $(addprefix $(stampdir)/stamp-build-,$(flavours))
+build-arch-deps-$(do_flavour_image_package) += $(addprefix $(stampdir)/stamp-install-,$(flavours))
 build-arch: $(build-arch-deps-true)
 	@echo Debug: $@
 

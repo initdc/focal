initdc/focal

commit a175d9b3c01899604c2c84492c1db99443248ee1

UBUNTU: [Packaging] Initial packaging import from linux-hwe-5.13
Initial packaging/config files imported from
Ubuntu-hwe-5.11-5.11.0-33.35_20.04.1.

Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

debian/rules.d/0-common-vars.mk
@@ -77,9 +77,7 @@ endif
 # The debug packages are ginormous, so you probably want to skip
 # building them (as a developer).
 #
-ifeq ($(full_build),false)
 skipdbg=true
-endif
 
 abinum		:= $(shell echo $(revision) | sed -r -e 's/([^\+~]*)\.[^\.]+(~.*)?(\+.*)?$$/\1/')$(abi_suffix)
 prev_abinum	:= $(shell echo $(prev_revision) | sed -r -e 's/([^\+~]*)\.[^\.]+(~.*)?(\+.*)?$$/\1/')$(abi_suffix)

commit f78a21022850dae39faa5dc78abd96b615dde34c

UBUNTU: [Packaging] Re-enable dbgsyms
BugLink: https://bugs.launchpad.net/bugs/1939287

During v5.13 cycle, debug symbols building got fixed by running
install step during build, instead of binary. Allowing to build
multiple flavours with debug symbols without breaching disk space on
Launchpad builder.

The packaging change that disabled dbgsyms unconditionally however
remained in the hwe-5.13 packaging, but can now be safely reverted to
reenable debug symbols builds.

Fixes: a175d9b3c0 ("UBUNTU: [Packaging] Initial packaging import from linux-hwe-5.13")
Link: https://bugs.launchpad.net/bugs/1930713
Acked-by: Tim Gardner <tim.gardner@canonical.com>
Acked-by: Andrea Righi <andrea.righi@canonical.com>
Signed-off-by: Dimitri John Ledkov <dimitri.ledkov@canonical.com>

debian/rules.d/0-common-vars.mk
@@ -77,7 +77,9 @@ endif
 # The debug packages are ginormous, so you probably want to skip
 # building them (as a developer).
 #
+ifeq ($(full_build),false)
 skipdbg=true
+endif
 
 abinum		:= $(shell echo $(revision) | sed -r -e 's/([^\+~]*)\.[^\.]+(~.*)?(\+.*)?$$/\1/')$(abi_suffix)
 prev_abinum	:= $(shell echo $(prev_revision) | sed -r -e 's/([^\+~]*)\.[^\.]+(~.*)?(\+.*)?$$/\1/')$(abi_suffix)